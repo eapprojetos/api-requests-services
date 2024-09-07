db8741659b8f4860da527a0085abece11025aee3
Eduardo Pacheco <epacheco.dev@gmail.com>
2024-09-07 10:52:35 -0300

    implement insert in database request services

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
+import database from "./database";
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ server.use(express.json());
 
 const port = process.env.PORT || 3001;
 
-server.get("/request/service", (req: Request, res: Response) => {
+server.post("/request/service", async (req: Request, res: Response) => {
     const { name, email, tell, service, terms_accepted } = req.body;
 
     const form = {
@@ -16,10 +17,22 @@ server.get("/request/service", (req: Request, res: Response) => {
         email,
         tell,
         service,
-        terms_accepted,
+        terms_accepted: !!terms_accepted,
     };
 
-    res.send(form).status(201);
+    try {
+        const formCreate = await database.request_service.create({
+            data: form,
+        });
+        res.send(formCreate).status(201);
+    } catch (error) {
+        console.error(error);
+        const data = {
+            status_code: 500,
+            error: "Erro ao tenta realizar o envio do formulario!",
+        };
+        res.send(data).status(500);
+    }
 });
 
 server.listen(port, () => {
