d47748fc92269889ff51b2de7cd90834f6d49858
Eduardo Pacheco <epacheco.dev@gmail.com>
2024-09-06 21:02:33 -0300

    create endpoint request service

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import dotenv from "dotenv";
 import express, { Request, Response } from "express";
 
-const server = express();
 dotenv.config();
+
+const server = express();
+server.use(express.json());
 
 const port = process.env.PORT || 3001;
 
-server.get("/", (req: Request, res: Response) => {
-    res.send(200);
+server.get("/request/service", (req: Request, res: Response) => {
+    const { name, email, tell, service, terms_accepted } = req.body;
+
+    const form = {
+        name,
+        email,
+        tell,
+        service,
+        terms_accepted,
+    };
+
+    res.send(form).status(201);
 });
 
 server.listen(port, () => {
